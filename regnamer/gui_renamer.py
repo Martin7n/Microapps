403170b1d0c624091dc42ac839e887bf96b64f7a
Martin7n <122726440+Martin7n@users.noreply.github.com>
2024-10-07 22:17:28 +0300

    micro app for file renaming conv.

---
+++ regnamer/gui_renamer.py
@@ -0,0 +1,44 @@
+from gui_renamer_func import file_rename
+import FreeSimpleGUI as sg
+import os
+
+label_1= "Select start folder"
+folder_input = sg.Input(default_text='C:\\ren\\')
+files_btn = sg.FolderBrowse("Add pdf folder", key="startfolder")
+
+
+
+label_3 = "Select registry file"
+reg_input = sg.Input(default_text="C:\\rename\\reg.xlsx")
+file_input_reg = sg.FileBrowse("Reg. file", key="reg_file")
+
+start_btn = sg.Button("Rename")
+status_label = sg.Text("")
+
+
+
+window = sg.Window("Renamer",
+                   layout=[[folder_input, files_btn],
+                           # [folder_input, fldr_btn],
+                           [reg_input, file_input_reg],
+                           [start_btn, status_label],
+                           ],
+                   font=("Helvetica", 15))
+while True:
+
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    # print(values, values)
+
+    filepaths = values['startfolder']
+    reg_file = values['reg_file']
+    # print(f"StartFolder {filepaths} reg_file ==> {reg_file}")
+
+    x_files = file_rename(filepaths, reg_file)
+
+    status_label.update(value=f"Rename completed: {x_files} renamed")
+
+
+
+window.close()
---
+++ regnamer/gui_renamer_func.py
@@ -0,0 +1,63 @@
+import os
+from pprint import pprint
+import FreeSimpleGUI as sg
+import openpyxl
+from openpyxl import Workbook
+
+def file_rename(filepaths,  reg_file):
+    try:
+
+        files_in_directory = os.listdir(filepaths)
+        # reg_file = "C:/ren/ren/reg.xlsx"
+        reg_file = reg_file
+        wb_obj = openpyxl.load_workbook(reg_file)
+        sheet_obj = wb_obj.active
+        max_row = sheet_obj.max_row
+        register = {}
+        for row in sheet_obj.iter_rows(min_row=2, max_col=4, max_row=max_row):
+            appendix = str(row[0].value) + row[1].value + str(row[2].value)
+            reg = row[3].value
+            register[reg] = appendix
+
+        names = []
+        for file in files_in_directory:
+            names.append(file)
+
+        count = 0
+        for name in names:
+            search_pattern = name[:8]
+            # print(search_pattern)
+            if search_pattern in register:
+                new_name = register[search_pattern] + "_" + name
+                count += 1
+                # print(new_name)
+                # old_file = os.path.join(directory_path, name)
+                # new_file = os.path.join(directory_path, new_name)
+
+                old_file = filepaths + "//" + name
+                new_file = filepaths + "//" + new_name
+
+                os.rename(old_file, new_file)
+                with open("new.txt", "a") as file:
+                    file.writelines(f"{name}, {new_name}")
+        return count
+    except Exception:
+        exit()
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
