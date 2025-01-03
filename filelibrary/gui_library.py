65e780a68a214a0d85242b0c08d6dcfb079402a5
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-01-03 18:58:12 +0200

    adding filter for some filetypes

--- filelibrary/gui_library.py
+++ filelibrary/gui_library.py
@@ -6,14 +6,13 @@ label_1= "Select start folder"
 folder_input = sg.Input(default_text='Browse to select a folder')
 start_btn = sg.Button("Scan & Save")
 files_btn = sg.FolderBrowse("Add pdf folder", key="startfolder")
-
-# start_btn = sg.Button("Create a registry of the files")
+type_btn = sg.Checkbox("Select All types", default=False, key="type")
 
 status_label = sg.Text("")
 
 
 window = sg.Window("File library",
-                   layout=[[folder_input,files_btn],
+                   layout=[[folder_input,files_btn], [type_btn],
                            [start_btn, status_label],
                            ],
                    font=("Helvetica", 15))
@@ -22,9 +21,11 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
     try:
         filepaths = values['startfolder']
-        status_label.update(value=f"Process completed {file_index(filepaths)} found")
+        types = values['type']
+        status_label.update(value=f"Process completed {file_index(filepaths,types )} found")
     except:
         status_label.update(value=f"Enter a valid path")
 
--- filelibrary/indexator.py
+++ filelibrary/indexator.py
@@ -1,7 +1,7 @@
 import os
 import openpyxl
 
-def file_index(path_to_dir):
+def file_index(path_to_dir, filetypes:bool):
     wb = openpyxl.Workbook()
     sheet = wb.active
     sheet.cell(row=1, column=1).value = "Path"
@@ -9,10 +9,8 @@ def file_index(path_to_dir):
     sheet.cell(row=1, column=3).value = "Name"
     sheet.cell(row=1, column=4).value = "Size"
     sheet.cell(row=1, column=5).value = "Type"
-
-
+    sheet.cell(row=1, column=6).value = "Direct link"
     wb.save('files_found.xlsx')
-
 
     files_in_directory = os.listdir(path_to_dir)
     count = 0
@@ -22,17 +20,39 @@ def file_index(path_to_dir):
             size = os.path.getsize(found)
             created =   os.path.getctime(found)
             type = f"<{name.split(".")[-1]}>"
-            sheet.cell(row=row, column=1).value = path
-            sheet.cell(row=row, column=2).value = found
-            sheet.cell(row=row, column=3).value = name
-            sheet.cell(row=row, column=4).value = size
-            sheet.cell(row=row, column=5).value = type
-            count += 1
+            docs_and_others = ["pdf", "doc", "docx", "jpg", "jpeg", "png", "gif", "webp", "xls", "xlsx", "eml"]
+
+            if not filetypes:
+                if name.split(".")[-1] in docs_and_others:
+                    sheet.cell(row=row, column=1).value = path
+                    sheet.cell(row=row, column=2).value = found
+                    sheet.cell(row=row, column=3).value = name
+                    sheet.cell(row=row, column=4).value = size
+                    sheet.cell(row=row, column=5).value = type
+                    sheet.cell(row=row, column=6).value = name
+                    sheet.cell(row=row, column=6).hyperlink = found
+                    sheet.cell(row=row, column=6).style = "Hyperlink"
+                    count += 1
+            else:
+                sheet.cell(row=row, column=1).value = path
+                sheet.cell(row=row, column=2).value = found
+                sheet.cell(row=row, column=3).value = name
+                sheet.cell(row=row, column=4).value = size
+                sheet.cell(row=row, column=5).value = type
+                sheet.cell(row=row, column=6).value = name
+                sheet.cell(row=row, column=6).hyperlink = found
+                sheet.cell(row=row, column=6).style = "Hyperlink"
+                count += 1
+
 
     wb.save('files_found.xlsx')
     wb.close()
     return count
 
+
+def file_writer(path, found, name, size, type):
+    wb = openpyxl.Workbook()
+    sheet = wb.active
 
 if __name__ == '__main__':
     print(file_index('C:\\Users\\'))
